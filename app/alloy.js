88b6031f3e5abd24c99f70dcfbc2f8da1d35d614
ptquang86 <ptquang86@gmail.com>
2016-04-06 10:54:12 +0700

    update alloy.js

--- app/alloy.js
+++ app/alloy.js
@@ -1,30 +1,42 @@
+// /*
 (function() {
 	
-	/*
-	var displayCaps = Ti.Platform.displayCaps,
-		platformWidth = displayCaps.platformWidth,
-		platformHeight = displayCaps.platformHeight;
+	// var displayCaps = Ti.Platform.displayCaps,
+		// platformWidth = displayCaps.platformWidth,
+		// platformHeight = displayCaps.platformHeight;
+	// Ti.API.info('ScreenSize: ' + JSON.stringify( [platformWidth, platformHeight] ));
+	// if (OS_ANDROID) {
+		// var measurement = require('alloy/measurement');
+		// platformWidth = Math.floor(measurement.pxToDP(platformWidth));
+		// platformHeight = Math.floor(measurement.pxToDP(platformHeight));
+		// Ti.API.info('ScreenSize: ' + JSON.stringify( [platformWidth, platformHeight] ));
+		// Ti.API.info('if ( ScreenSize == ScreenSize scaled ) { Delete the [build] + [Resources] folders and build again. }'); 
+	// }
 	
-	Ti.API.info('ScreenSize 1: ' + JSON.stringify( [platformWidth, platformHeight] ));
-	
-	if (OS_ANDROID) {
-		var measurement = require('alloy/measurement');
-		platformWidth = Math.floor(measurement.pxToDP(platformWidth));
-		platformHeight = Math.floor(measurement.pxToDP(platformHeight));
-		
-		Ti.API.info('ScreenSize 2: ' + JSON.stringify( [platformWidth, platformHeight] ));
-		
-		if (platformWidth == displayCaps.platformWidth && platformHeight == displayCaps.platformHeight) {
-			Ti.API.error('alloy.js: Delete the [build] + [Resources] folders then build again.');
-		}
-	}
-	*/
+  	// var originalWidth = 375;
+	// var IsScreen320 = false,
+		// IsScreen375 = false,
+		// ScreenScale = null;
+	// if (platformWidth < originalWidth) {
+		// if (platformWidth <= 320) {
+			// IsScreen320 = true;
+		// } else if (platformWidth <= 375) {
+			// IsScreen375 = true;
+		// }
+		// var scaleFactor = (platformWidth / originalWidth).toFixed(1);
+		// ScreenScale = Ti.UI.create2DMatrix().scale(scaleFactor, scaleFactor);
+	// }
 	
   	Alloy.Globals.UI = {
+  		// IsScreen320: IsScreen320,
+  		// IsScreen375: IsScreen375,
+  		// ScreenScale: ScreenScale,
   		// Win: {
+  			// Short: platformHeight <= 480,
   			// Width: platformWidth,
   			// Height: platformHeight
   		// }
   	};
   	
 })();
+// */
