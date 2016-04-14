810fd7182a18e1b73480907c7931ae98af7b5cb6
ptquang86 <ptquang86@gmail.com>
2016-04-14 10:51:30 +0700

    update alloy.js

--- app/alloy.js
+++ app/alloy.js
@@ -13,15 +13,21 @@
 		// Ti.API.info('if ( ScreenSize == ScreenSize scaled ) { Delete the [build] + [Resources] folders and build again. }'); 
 	// }
 	
-  	// var originalWidth = 375;
-	// var IsScreen320 = false,
-		// IsScreen375 = false,
+  	// var originalWidth = 414;
+	// var IsScreen320 = false, //  640 × 960
+		// IsScreen375 = false, //  750 × 1334
+		// IsScreen414 = false, // 1242 × 2208
+		// IsScreenBig = false, // > 1242
 		// ScreenScale = null;
 	// if (platformWidth < originalWidth) {
 		// if (platformWidth <= 320) {
 			// IsScreen320 = true;
 		// } else if (platformWidth <= 375) {
 			// IsScreen375 = true;
+		// } else if (platformWidth <= 414) {
+			// IsScreen414 = true;
+		// } else {
+			// IsScreenBig = true;
 		// }
 		// var scaleFactor = (platformWidth / originalWidth).toFixed(1);
 		// ScreenScale = Ti.UI.create2DMatrix().scale(scaleFactor, scaleFactor);
@@ -30,6 +36,8 @@
   	Alloy.Globals.UI = {
   		// IsScreen320: IsScreen320,
   		// IsScreen375: IsScreen375,
+  		// IsScreen414: IsScreen414,
+  		// IsScreenBig: IsScreenBig,
   		// ScreenScale: ScreenScale,
   		// Win: {
   			// Short: platformHeight <= 480,
