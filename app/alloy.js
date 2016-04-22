866480bbba1c072bb0d148c8b2e141728b08f617
ptquang86 <ptquang86@gmail.com>
2016-04-22 08:59:20 +0700

    update styles

--- app/alloy.js
+++ app/alloy.js
@@ -4,34 +4,34 @@
 	// var displayCaps = Ti.Platform.displayCaps,
 		// platformWidth = displayCaps.platformWidth,
 		// platformHeight = displayCaps.platformHeight;
-	// Ti.API.info('ScreenSize: ' + JSON.stringify( [platformWidth, platformHeight] ));
+	// Ti.API.info('ScreenSize 1: ' + JSON.stringify( [platformWidth, platformHeight] ));
 	// if (OS_ANDROID) {
 		// var measurement = require('alloy/measurement');
 		// platformWidth = Math.floor(measurement.pxToDP(platformWidth));
 		// platformHeight = Math.floor(measurement.pxToDP(platformHeight));
-		// Ti.API.info('ScreenSize: ' + JSON.stringify( [platformWidth, platformHeight] ));
-		// Ti.API.info('if ( ScreenSize == ScreenSize scaled ) { Delete the [build] + [Resources] folders and build again. }'); 
+		// Ti.API.info('ScreenSize 2: ' + JSON.stringify( [platformWidth, platformHeight] ));
+		// if (platformWidth == displayCaps.platformWidth && platformHeight == displayCaps.platformHeight) {
+			// Ti.API.error('alloy.js: Delete the [build] + [Resources] folders then build again.');
+		// }
 	// }
 	
-  	// var originalWidth = 414;
 	// var IsScreen320 = false, //  640 × 960
 		// IsScreen375 = false, //  750 × 1334
 		// IsScreen414 = false, // 1242 × 2208
-		// IsScreenBig = false, // > 1242
-		// ScreenScale = null;
-	// if (platformWidth < originalWidth) {
-		// if (platformWidth <= 320) {
-			// IsScreen320 = true;
-		// } else if (platformWidth <= 375) {
-			// IsScreen375 = true;
-		// } else if (platformWidth <= 414) {
-			// IsScreen414 = true;
-		// } else {
-			// IsScreenBig = true;
-		// }
-		// var scaleFactor = (platformWidth / originalWidth).toFixed(1);
-		// ScreenScale = Ti.UI.create2DMatrix().scale(scaleFactor, scaleFactor);
+		// IsScreenBig = false; // > 1242
+	// if (platformWidth <= 320) {
+		// IsScreen320 = true;
+	// } else if (platformWidth <= 375) {
+		// IsScreen375 = true;
+	// } else if (platformWidth <= 414) {
+		// IsScreen414 = true;
+	// } else {
+		// IsScreenBig = true;
 	// }
+	
+	// var originalWidth = 414;
+	// var scaleFactor = (platformWidth / originalWidth).toFixed(1);
+	// var ScreenScale = Ti.UI.create2DMatrix().scale(scaleFactor, scaleFactor);
 	
   	Alloy.Globals.UI = {
   		// IsScreen320: IsScreen320,
